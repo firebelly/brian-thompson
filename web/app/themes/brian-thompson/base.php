1c8dccb2760509aa46a66cf05998da30c748d67b
Bryant Smith <bryant.robert.smith@gmail.com>
2016-06-28 14:13:57 -0500

    Experimental typography system that obeys baseline and margins

--- web/app/themes/brian-thompson/base.php
+++ web/app/themes/brian-thompson/base.php
@@ -16,11 +16,11 @@ use Roots\Sage\Wrapper;
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
-    <div class="site-wrap">
       <?php
         do_action('get_header');
         get_template_part('templates/header');
       ?>
+    <div class="main-area-wrap">
       <div class="body-wrap" role="document">
         <main class="site-main" role="main">
           <?php include Wrapper\template_path(); ?>
--- web/app/themes/brian-thompson/templates/header.php
+++ web/app/themes/brian-thompson/templates/header.php
@@ -1,16 +1,24 @@
 <header class="site-header" role="banner">
-    <h1 class="brand"><a href="<?= esc_url(home_url('/')); ?>">
-      <svg class="btf-logo" role="img" width="101" height="53"><use xlink:href="#btf-logo"></use></svg>
-    </a></h1>
-    <nav class="site-nav" role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu([
-          'theme_location' => 'primary_navigation', 
-          'menu_class' => 'nav',
-          'depth' => 2,
-        ]);
-      endif;
-      ?>
-    </nav>
+  <div class="brand-wrap">
+    <div class="main-area-wrap">
+      <h1 class="brand"><a href="<?= esc_url(home_url('/')); ?>">
+        <svg class="btf-logo" role="img" width="101" height="53"><use xlink:href="#btf-logo"></use></svg>
+      </a></h1>
+    </div>
+  </div>
+  <div class="site-nav-wrap">
+    <div class="main-area-wrap">
+      <nav class="site-nav" role="navigation">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu([
+            'theme_location' => 'primary_navigation', 
+            'menu_class' => 'nav',
+            'depth' => 2,
+          ]);
+        endif;
+        ?>
+      </nav>
+    </div>
+  </div>
 </header>
