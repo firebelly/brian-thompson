a59ef44af4da1c861f013d7732bca3b97418f21f
Bryant Smith <bryant.robert.smith@gmail.com>
2016-07-18 13:03:24 -0500

    A few styling fixes

--- web/app/themes/brian-thompson/404.php
+++ web/app/themes/brian-thompson/404.php
@@ -1,4 +1,2 @@
 <?php get_template_part('templates/page', 'header'); ?>
-
-<h1> Page Not Found </h1>
 <p> How about some <a href="<?=get_permalink( get_option('page_for_posts' ) )?>">light reading</a> instead? </p>
