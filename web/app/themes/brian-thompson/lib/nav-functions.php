a1a4926632a0584862fc77cfdf82a705a507fd69
Bryant Smith <bryant.robert.smith@gmail.com>
2016-08-02 16:30:50 -0500

    Properly highlight search in nav

--- web/app/themes/brian-thompson/lib/nav-functions.php
+++ web/app/themes/brian-thompson/lib/nav-functions.php
@@ -6,7 +6,9 @@ namespace Firebelly\Navs;
  * Add current-menu-item class to blog link if we are on an archive page
  */
 function highlight_blog_link( $classes, $item ) {
-  if( ( is_single() || is_category() || is_search() ) && $item->object_id===get_option('page_for_posts') )
+  if( ( is_single() || is_category() ) && $item->object_id===get_option('page_for_posts') )
+    $classes[] = "current-menu-item";
+  if( ( is_search() ) && $item->url==='#search' )
     $classes[] = "current-menu-item";
   return $classes;
 }
