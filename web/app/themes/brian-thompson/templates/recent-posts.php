d001e4aa241f22f6bc51d13a53bba403bff88896
Matt Soria <matt.m.soria@gmail.com>
2017-03-16 16:11:50 -0500

    Doing a check in the recent-posts partial for if posts exist

--- web/app/themes/brian-thompson/templates/recent-posts.php
+++ web/app/themes/brian-thompson/templates/recent-posts.php
@@ -1,13 +1,17 @@
+<?php
+$args = [
+  'post_type'   => 'post',
+  'numberposts' => 2,
+  'orderby'     => 'date',
+];
+$recent_posts = get_posts($args);
+?>
+
+<?php if (!empty($recent_posts)) { ?>
 <div class="recent-posts">
   <h4><?php _e('Latest News', 'sage'); ?></h3>
     <ul class="posts">
       <?php
-      $args = [
-        'post_type'   => 'post',
-        'numberposts' => 2,
-        'orderby'     => 'date',
-      ];
-      $recent_posts = get_posts($args);
       foreach ($recent_posts as $recent_post) : 
         ?>
         <li class="post">
@@ -16,3 +20,4 @@
       <?php endforeach; ?>
     </ul>
 </div>
+<?php } ?>
