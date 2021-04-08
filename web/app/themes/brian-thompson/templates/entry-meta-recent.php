5d6e886f29862deecd2a85a09957f48a1e088a75
itsmattsoria <matt.m.soria@gmail.com>
2021-04-08 14:24:14 -0500

    Adjusting styles of recent posts on single podcast template

--- web/app/themes/brian-thompson/templates/entry-meta-recent.php
+++ web/app/themes/brian-thompson/templates/entry-meta-recent.php
@@ -1,9 +1,9 @@
-<?php 
-use Firebelly\Utils; 
+<?php
+use Firebelly\Utils;
 $category = Utils\get_category( $post );
 ?>
 
 <div class="meta">
   <p class="category"><?php if ($category) : ?><a href="<?= get_term_link($category); ?>" class="no-underline"><?= $category->name; ?></a><?php endif; ?></p>
-  <p class="date"><time datetime="<?= date('c', strtotime($post->post_date)); ?>"><?= date('m.d', strtotime($post->post_date)); ?></time></p>
+  <p class="date"><time datetime="<?= date('c', strtotime($post->post_date)); ?>"><?= date('m.d.y', strtotime($post->post_date)); ?></time></p>
 </div>
